c04749f473a9fa33956c49d83cb2c8b1a6eea5dc
ayush20021 <84669540+ayush20021@users.noreply.github.com>
2023-11-08 08:08:36 +0530

    Update Strings.java

--- LBSS/LBS/src/practise/Strings.java
+++ LBSS/LBS/src/practise/Strings.java
@@ -241,4 +241,32 @@ public class Strings {
 
 
     }
+
+
+}
+public static void main(String[] args) {
+    String str = "aaaaaakaaaaaa";
+    int count = 0;
+
+    for (int i = 0; i < str.length(); i++) {
+        char currentChar = str.charAt(i);
+        boolean isUnique = true;
+
+        for (int j = 0; j < str.length(); j++) {
+            if (i != j && currentChar == str.charAt(j)) {
+                isUnique = false;
+                break;
+            }
+        }
+
+        if (isUnique) {
+            System.out.println(currentChar);
+            count++;
+            break;
+        }
+    }
+
+    if (count == 0) {
+        System.out.println("No unique characters found.");
+    }
 }
